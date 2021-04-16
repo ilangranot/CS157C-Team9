1b8e40f658000fffe2989bd52144d3e60826e352
nick-kim-96 <nickthuazn@gmail.com>
2021-04-15 23:44:50 -0700

    Added function to create nodes and a relationship between them

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,6 +12,7 @@ import org.neo4j.driver.exceptions.Neo4jException;
 import static org.neo4j.driver.Values.parameters;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 public class App implements AutoCloseable{
@@ -71,13 +72,43 @@ public class App implements AutoCloseable{
         return returnVal;
     }
     
+    public void createNavigation(final String url1, final String url2, final String user_id, final String date) {
+    	String createNavigationQuery = "CREATE (n1:website {url: $url1, user_id: $user_id, date: $date})\n" +
+    			"CREATE (n2:website {url: $url2, user_id: $user_id, date: $date})\n" + 
+    			"CREATE (n1)-[:NAVTO2]->(n2)\n" +
+    			"RETURN n1, n2";
+    	
+    	Map<String, Object> params = new HashMap<>();
+        params.put("url1", url1);
+        params.put("url2", url2);
+        params.put("user_id", user_id);
+        params.put("date", date);
+
+        try (Session session = driver.session()) {
+            // Write transactions allow the driver to handle retries and transient errors
+            Record record = session.writeTransaction(tx -> {
+                Result result = tx.run(createNavigationQuery, params);
+                return result.single();
+            });
+            System.out.println(String.format("Created friendship between: %s, %s",
+                    record.get("n1").get("url").asString(),
+                    record.get("n2").get("url").asString()));
+        // You should capture any errors along with the query and data for traceability
+        } catch (Neo4jException ex) {
+            throw ex;
+        }
+    }
+    
 
     public static void main( String... args ) throws Exception
     {
+    	String url1 = "https://sjsu.instructure.com/courses/1377088";
+    	String url2 = "https://sjsu.instructure.com/courses/1377088/modules";
         try ( App greeter = new App( "bolt://localhost:7687", "neo4j", "password" ) )
         {
             greeter.printGreeting( "hello, world" );
-            greeter.getAll("https://sjsu.instructure.com/courses/1377088/modules");
+            //greeter.checkNode("https://sjsu.instructure.com/courses/1377088/modules");
+            greeter.createNavigation(url1, url2, "9", "07/15/2020");
         }
     }
 }
