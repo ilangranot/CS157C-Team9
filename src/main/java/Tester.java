f1c600ea6430454c36f93a8c5eb361c538825e56
sebriannef <sebriannef@gmail.com>
2021-04-18 01:28:21 -0700

    tester file + WUMGrapher additions

--- src/main/java/Tester.java
+++ src/main/java/Tester.java
@@ -3,11 +3,8 @@
  * Authors: Ilan Granot, Nick Kim, Sebrianne Ferguson, Mike Wu
  * Copyright © 2021 Granot et. al.
  *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation to
- * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
- * sell copies of the Software.
+ * This file is subject to the terms and conditions defined in
+ * the file 'LICENSE.txt', which is part of the source code package.
  *
  * Tester.java
  * Purpose: test the functionality of the API for Web Usage Mining of Frequent Patterns
