52fce5e6cf3c4517f68d7c267fdfe119101b7874
nick-kim-96 <nickthuazn@gmail.com>
2021-05-07 20:06:47 -0700

    small fix

--- src/main/java/GraphStats.java
+++ src/main/java/GraphStats.java
@@ -3,7 +3,7 @@ public class GraphStats {
     private long linesRead;
     private long linesProcessed;
     private long distinctUsers;
-    private GraphStatus status;
+    private GraphStatus status = GraphStatus.STANDBY;
 
     public GraphStats() {
 
